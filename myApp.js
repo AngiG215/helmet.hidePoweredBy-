83b4c3801caa655a1c32cba552b74a50cec311ac
Angela <angieadrigarcia572@gmail.com>
2026-02-21 20:15:57 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,11 +2,11 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-app.use(helmet());
+// app.use(helmet());
 
 
 
-// app.use(helmet.hidePoweredBy());
+app.use(helmet.hidePoweredBy());
 
 
 
