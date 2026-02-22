b3afdfb2ebcac4cb6084c05c325464bf582c8ac8
Angela <angieadrigarcia572@gmail.com>
2026-02-21 21:55:04 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,15 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-// 1. Usa helmet para ocultar X-Powered-By
-app.use(helmet.hidePoweredBy());
+// ESTA ES LA ÚNICA LÍNEA QUE REALMENTE IMPORTA PARA EL RETO
+app.use(helmet.hidePoweredBy()); 
 
-// 2. Configura los archivos estáticos y la ruta de inicio
-app.use(express.static('public'));
-
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-// 3. EXPORTAR APP - Esto es lo único que debe ir al final
 module.exports = app;
